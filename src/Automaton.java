4dda9a093ad3bc58d9a0cd94e3f96ab6c263fa9d
AbdAlrrahmanGhumid <abbode.gho@gmail.com>
2024-05-29 13:39:09 +0200

    changed at 29.05.2024

---
+++ src/Automaton.java
@@ -0,0 +1,17 @@
+public class Automaton {
+    State strartingState = null;
+    State endingState = null;
+
+    public Automaton(State startingState, State endingState){
+        this.strartingState = startingState;
+        this.endingState = endingState;
+    }
+
+    public void addState(State state){
+
+    }
+
+    public void addTransition(Transition tra){
+
+    }
+}
